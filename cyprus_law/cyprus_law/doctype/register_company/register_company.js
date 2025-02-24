a149b5eb34cd420b6a9b79b129dec6abe74cd1d9
Panayiotis Halouvas <phalouvas@gmail.com>
2025-02-24 07:08:21 +0000

    feat: Implement dynamic fetching of related persons in Register Company doctype

--- cyprus_law/cyprus_law/doctype/register_company/register_company.js
+++ cyprus_law/cyprus_law/doctype/register_company/register_company.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, KAINOTOMO PH LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Register Company", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Register Company", {
+    refresh(frm) {
+        // Fetch related persons dynamically
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Related Person",
+                filters: [
+                    ["register_company", "=", frm.doc.name],
+                    ["disabled", "=", 0]
+                ],
+                fields: ["name", "person_name", "type"],
+                limit_page_length: 20
+            },
+            callback: function(r) {
+                if (r.message) {
+                    let html = "<table class='table table-bordered'><tr><th>ID</th><th>Name</th><th>Type</th></tr>";
+                    r.message.forEach(function(row) {
+                        html += `<tr><td>${row.name}</td><td>${row.person_name}</td><td>${row.type || ""}</td></tr>`;
+                    });
+                    html += "</table>";
+                    frm.set_df_property("company_structure_html", "options", html);
+                }
+            }
+        });
+    },
+});
