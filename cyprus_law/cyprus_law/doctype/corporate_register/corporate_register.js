8d61b28a703a0b6691a3a6ab79ac2d041408f640
Panayiotis Halouvas <phalouvas@gmail.com>
2025-02-24 15:17:54 +0000

    feat: Implement dynamic fetching of registered offices in Corporate Register doctype and enhance field structure

--- cyprus_law/cyprus_law/doctype/corporate_register/corporate_register.js
+++ cyprus_law/cyprus_law/doctype/corporate_register/corporate_register.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, KAINOTOMO PH LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Corporate Register", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Corporate Register", {
+    refresh(frm) {
+        // Fetch registered offices dynamically
+        frappe.call({
+            method: "frappe.client.get_list",
+            args: {
+                doctype: "Registered Office",
+                filters: [
+                    ["corporate_register", "=", frm.doc.name],
+                    ["disabled", "=", 0]
+                ],
+                fields: ["name", "address", "address_html"],
+                limit_page_length: 20
+            },
+            callback: function(r) {
+                if (r.message) {
+                    let registeredOfficesHtml = "<table class='table table-bordered'>";
+                    r.message.forEach(function(row) {
+                        registeredOfficesHtml += `<tr><td><a href="/app/registered-office/${row.name}">${row.address || ""}</a></td></tr>`;
+                    });
+                    registeredOfficesHtml += "</table>";
+                    frm.set_df_property("registered_offices_html", "options", registeredOfficesHtml);
+                }
+            }
+        });
+    },
+});
