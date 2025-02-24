a5ac99e888c032897735ee2164571378500ec81a
Panayiotis Halouvas <phalouvas@gmail.com>
2025-02-24 06:03:54 +0000

    feat: Enhance Related Person doctype with address and contact management

--- cyprus_law/cyprus_law/doctype/related_person/related_person.py
+++ cyprus_law/cyprus_law/doctype/related_person/related_person.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2025, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.contacts.address_and_contact import (
+	delete_contact_and_address,
+	load_address_and_contact,
+)
 
 
 class RelatedPerson(Document):
-	pass
+	
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
+
+	def on_trash(self):
+		delete_contact_and_address("Related Person", self.name)
